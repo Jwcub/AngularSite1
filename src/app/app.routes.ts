4bc15d1771877163583db69a07c53272cef132e6
Kevin.M <kema2501@student.miun.se>
2026-04-23 08:51:16 +0200

    Lägger till sidorna home, about, converter och 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Converter } from './converter/converter';
+import { About } from './about/about';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: Home },
+    { path: "converter", component: Converter },
+    { path: "about", component: About },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "**", component: NotFound },
+    { path: "404", component: NotFound }
+];
