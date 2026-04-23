ba83a38e9bdd91cb49d140529c5c66926018bd11
Kevin.M <kema2501@student.miun.se>
2026-04-23 12:51:54 +0200

    Lägg till konverterare

--- src/app/converter/converter.ts
+++ src/app/converter/converter.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './converter.html',
   styleUrl: './converter.css',
 })
-export class Converter {}
+export class Converter {
+  value: number = 0;
+  method: string = "";
+  result: string = "";
+
+  convertValue():void {
+    if(this.method === "MtoI"){
+      this.result = meterToInches(this.value);
+    }
+
+    if(this.method === "CtoF") {
+      this.result = celsiusToFahrenheit(this.value);
+    }
+    else {
+      this.result = "Ingen metod vald"
+    }
+  }
+}
+
+function meterToInches(meters: number):string {
+  const inches = meters * 39.3701;
+  const message = `${meters} meter är ${inches} inches`;
+  return message
+}
+
+function celsiusToFahrenheit(celsius: number):string {
+  const fahrenheit = celsius * 1.8 + 32;
+  const message = `${celsius} Celsius är ${fahrenheit} Fahrenheit`
+  return message
+}
