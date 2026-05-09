9d67ec61da77bcd26e526fd358d3ea2c52abfac9
Kevin.M <kema2501@student.miun.se>
2026-05-09 17:13:58 +0200

    Lägg till omvandling för fot-meter samt fahrenheit - celsius

--- src/app/converter/converter.ts
+++ src/app/converter/converter.ts
@@ -7,44 +7,57 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './converter.html',
   styleUrl: './converter.css',
 })
+
 export class Converter {
   value: number = 0;
   method: string = "";
   result: string = "";
 
-  convertValue():void {
+  convertMethod():void {
     if(this.method === "MtoF"){
-      this.result = meterToFeet(this.value);
+      this.result = this.meterToFeet(this.value);
+    }
+
+    if(this.method === "FtoM"){
+      this.result = this.feetToMeter(this.value);
     }
 
     if(this.method === "CtoF") {
-      this.result = celsiusToFahrenheit(this.value);
+      this.result = this.celsiusToFahrenheit(this.value);
     }
 
-    if(this.method === "KgtoLbs") {
-      this.result = kilogramToPounds(this.value);
+    if(this.method === "FtoC") {
+      this.result = this.fahrenheitToCelsius(this.value);
     }
+
     else return
+  }
+
+  meterToFeet(meters: number):string {
+    const feet = meters * 3.28084;
+    const message = `${meters} meter är ${feet} fot`;
+    return message
+  }
+
+  feetToMeter(feet: number):string {
+    const meters = feet * 0.3048;
+    const message = `${feet} meter är ${meters} fot`;
+    return message
+  }
+
+  // Funktion för omvandling till fahrenheit
+  celsiusToFahrenheit(celsius: number):string {
+    const fahrenheit = celsius * 1.8 + 32;
+    const message = `${celsius} Celsius är ${fahrenheit} Fahrenheit`;
+    return message
+  }
+
+  // Funktion för omvandling till fahrenheit
+  fahrenheitToCelsius(fahrenheit: number):string {
+    const celsius = (fahrenheit - 32) * 5/9;
+    const message = `${fahrenheit} Fahrenheit är ${celsius} Celsius`;
+    return message
   }
 }
 
-// Funktion för omvandling till fot
-function meterToFeet(meters: number):string {
-  const feet = meters * 3.28084
-  const message = `${meters} meter är ${feet} fot`;
-  return message
-}
 
-// Funktion för omvandling till fahrenheit
-function celsiusToFahrenheit(celsius: number):string {
-  const fahrenheit = celsius * 1.8 + 32;
-  const message = `${celsius} Celsius är ${fahrenheit} Fahrenheit`;
-  return message
-}
-
-// Funktion för omvandling tull pounds
-function kilogramToPounds(kg: number): string {
-  const pounds = kg * 2.20462;
-  const message = `${kg} kilogram är ${pounds} lbs`;
-  return message
-}
